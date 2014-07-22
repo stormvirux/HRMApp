9e2d55b49057c11c8b12707a468a7116ac21f2bb
Thaha Muhammed <mthaha1989@gmail.com>
2014-07-22 15:40:37 +0530

    Done with login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140722051712) do
+ActiveRecord::Schema.define(:version => 20140722093637) do
 
   create_table "contracts", :force => true do |t|
     t.string   "empno"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20140722051712) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "remember_token"
   end
 
 end
