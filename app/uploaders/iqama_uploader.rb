146b300f4947828a4004f4e2c07c4bd10dbd7fdf
Thaha Muhammed <mthaha1989@gmail.com>
2014-08-01 13:28:22 +0530

    implemented vehicle db and doc img upload

--- app/uploaders/iqama_uploader.rb
+++ app/uploaders/iqama_uploader.rb
@@ -3,7 +3,7 @@
 class IqamaUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -38,9 +38,9 @@ class IqamaUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
