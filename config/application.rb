b1a81340d186fd56f8fc0a077f93fc50de499aee
Thaha Muhammed <mthaha1989@gmail.com>
2014-08-03 15:20:23 +0530

    change in login page design

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 
 module HrsiteNew
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("vendor","assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
