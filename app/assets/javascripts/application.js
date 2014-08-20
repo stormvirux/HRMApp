806756a5cbb2212a2e37430df6589d917c7de9cd
Thaha Muhammed <mthaha1989@gmail.com>
2014-08-20 18:07:17 +0530

    ready heroku after Complete image upload using carrierwave

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require cloudinary
-//= require twitter/bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require_tree .
 
 
