d3202904e295ca600598e5039cbb97ea9373e8ad
Thaha Muhammed <mthaha1989@gmail.com>
2014-08-20 08:20:33 +0530

    get it ready for hroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ HrsiteNew::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.precompile += %w( *.css *.js )
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
